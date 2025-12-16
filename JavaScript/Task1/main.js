78787f8a142f4bc0a8fa8d6c7c82c1529ecd468c
Doaa Arfa <168927199+doaamagdy12@users.noreply.github.com>
2025-12-16 14:40:15 +0200

    Add files via upload

---
+++ JavaScript/Task1/main.js
@@ -0,0 +1,89 @@
+// Problem 1
+
+let x = 5;
+let y = 4;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y+3);
+console.log((x+y)*2);
+console.log(x/y);
+console.log(x%y);
+console.log((x-2)*(y+1));
+
+// Problem 2
+
+let a = 10 ;
+let b = "Hello";
+let c = true ;
+let d = {name : "Backend"};
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+
+// Problem 3
+let x2 = 7;
+let y2 = 10;
+
+console.log(x2>y2);
+console.log(x2>=y2);
+console.log(x2<y2);
+console.log(x2<=y2);
+console.log(x2==y2);
+console.log(x2===y2);
+console.log(x2!=y2);
+console.log(x2!==y2);
+
+// Problem 4
+
+let a2 = true;
+let b2 = false;
+
+console.log(a2&&b2);
+console.log(a2||b2);
+console.log((a2&& !b2));
+console.log((!a2 || b2));
+
+// Problem 5
+
+let num =20;
+let str=" Years old";
+
+console.log(num+str);
+console.log("Age :" , num);
+console.log("Result = " , num+5);
+console.log("20"+10);
+console.log((20+10), "Days");
+console.log("JS" + " " + "Course");
+
+// Problem 6
+
+let n = 10;
+console.log(n+=5);
+console.log(n-=3);
+console.log(n*=2);
+console.log(n/=5);
+console.log(n%=4);
+
+// Problem 7
+
+let x3 = 8;
+++x3;
+x3++;
+console.log(x3);
+-- x3;
+x--;
+console.log(x3);
+
+// Problem 8
+let a3 = 4;
+let b3 = 2;
+let c3 = "3";
+
+console.log(a3+b3+c3);
+console.log(a3+(b3+c3));
+console.log((a3+b3) * Number(c3));
+console.log(a3+b3 * c3);
+
